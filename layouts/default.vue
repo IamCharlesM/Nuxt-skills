37a833004103cf63062c0bf6f5b0032e718112c0
IamCharlesM <rafroyevans@gmail.com>
2022-08-12 15:56:29 -0500

    Baseline pages a formatting

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,39 +25,21 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      
+<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title v-text="title" class=" mx-auto text-center" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam perferendis at qui velit itaque voluptas harum facere nostrum ipsa dignissimos deleniti asperiores, provident veritatis ex, quae eius saepe repellat eaque?</span>
     </v-footer>
   </v-app>
 </template>
@@ -72,20 +54,23 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-home',
           title: 'Welcome',
           to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'mdi-information-outline',
+          title: 'Description',
+          to: '/description',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'About Me',
+          to: '/about',
         },
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'General Skills',
     }
   },
 }
