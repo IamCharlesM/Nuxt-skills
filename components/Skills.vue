9c0cc0670cf8e5cefab6e3657cd3e8eb2ba653b8
IamCharlesM <rafroyevans@gmail.com>
2022-08-12 21:15:42 -0500

    More styling

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="hello">
     <v-row no-gutters>
-      <v-col>
+      <v-col class="mx-1">
         <h1>Vue</h1>
+
+        <form @submit.prevent="addSkill">
+          <input
+            type="text"
+            placeholder="Enter a skill you have"
+            v-model="skill"
+          />
+        </form>
+
         <ul>
-          <li v-for="(data, index) in skills" :key="index">
+          <li v-for="(data, index) in skills" :key="index" class="py-4">
             {{ data.skill }}
           </li>
         </ul>
 
-        <p>These are the skills that you possess</p>
+        <p>These are the skills that I possess</p>
       </v-col>
 
-      <v-col>
+      <v-col class="mx-1">
         <h1>Vuetify</h1>
+
+        <v-form @submit.prevent="addSkillVuetify">
+          <v-text-field
+            label="Enter a skill you have"
+            hide-details="auto"
+            v-model="skillVuetify"
+            outlined
+            class="my-5"
+          ></v-text-field>
+        </v-form>
+
         <v-list v-for="(data, index) in skills" :key="index">
-          <v-card class="py-5"> {{ data.skill }} </v-card>
+          <v-card class="py-4"> {{ data.skill }} </v-card>
         </v-list>
+        <v-alert type="info" dense>
+          These are the skills that I possess
+        </v-alert>
       </v-col>
     </v-row>
   </div>
@@ -27,9 +50,20 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
+      skillVuetify: '',
       skills: [{ skill: 'Vue.js' }, { skill: 'Vuetify' }],
-      showAlert: true,
     }
+  },
+  methods: {
+    addSkill() {
+      this.skills.push({ skill: this.skill })
+      this.skill = ''
+    },
+    addSkillVuetify() {
+      this.skills.push({ skill: 'vuetify-' + this.skillVuetify })
+      this.skillVuetify = ''
+    },
   },
 }
 </script>
@@ -42,7 +76,6 @@ ul {
 }
 
 ul li {
-  padding: 20px;
   font-size: 1.3em;
   background-color: grey;
   border: 5px solid inherit;
@@ -52,11 +85,20 @@ ul li {
 
 p {
   text-align: center;
-  padding: 30px 0;
   color: gray;
 }
 
 .container {
   box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: 100%;
+  border: 2px solid grey;
+  padding: 10px;
+  margin: 21px 0px;
+  font-size: 1.3em;
+  background-color: inherit;
+  color: #687f7f;
 }
 </style>
